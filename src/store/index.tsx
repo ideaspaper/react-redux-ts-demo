a042723f3271578182471acd1cf958784c1eba1a
Daniel Kristianto Haryono <daniel.haryono@shopee.com>
2023-01-03 06:04:44 +0700

    Feat:   - Add redux-devtools-extension.

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,5 +1,7 @@
 import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 import middlewares from './middlewares';
-const store = createStore(reducers, middlewares);
+const composedEnhancer = composeWithDevTools(middlewares);
+const store = createStore(reducers, composedEnhancer);
 export default store;
